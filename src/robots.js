8d83fa1ee5cb9a410e2844d0a652c95d571cb65c
Odulaja Oluwadamilare O <damijustice1@gmail.com>
2022-01-04 01:17:03 +0100

    code clean up and adding robots.js

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+    {
+        id: 1,
+        name: "Damilare Justice",
+        username: "odb23",
+        email: "dami@gmail.com"
+    },
+    {
+        id: 2,
+        name: "Veum Ruth",
+        username: "veumr",
+        email: "veum.ruth@hotmail.com"
+    },
+    {
+        id: 3,
+        name: "Josiah Ullrich",
+        username: "odb23",
+        email: "josiah.ullrich@kuvalis.com"
+    },
+    {
+        id: 4,
+        name: "Aveum Grimes",
+        username: "aveum",
+        email: "aveum@grimes.org"
+    },
+    {
+        id: 5,
+        name: "Rory Dan ",
+        username: "rory",
+        email: "rory41@howell.com"
+    },
+    {
+        id: 6,
+        name: "Korbin Lubowitz",
+        username: "korl",
+        email: "korbin.lubowitz@koch.org"
+    },
+    {
+        id: 7,
+        name: "Christian S.",
+        username: "chris",
+        email: "hchristiansen@hotmail.com"
+    },
+    {
+        id: 8,
+        name: "Alexzander Schmitt",
+        username: "salex",
+        email: "alexzander.schmitt@yahoo.com"
+    },
+    {
+        id: 9,
+        name: "Elliot Braun",
+        username: "ebraun",
+        email: "ebraun@gmail.com"
+    },
+    {
+        id: 10,
+        name: "Elian Josh",
+        username: "ejosh",
+        email: "elian14@gmail.com"
+    },
+];
