e864172fd4e9547251400cb04b1016c847f68a26
Odulaja Oluwadamilare O <damijustice1@gmail.com>
2022-01-13 20:20:18 +0100

    project folder restructure

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox';
+import CardList from '../CardList';
+import SearchBox from '../SearchBox';
 import './App.css';
-import Scroll from './Scroll';
+import Scroll from '../Scroll';
 
 
 class App extends Component {
